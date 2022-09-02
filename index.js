fe1f5e93d01d12ff766ccdc140c34bce272a3047
sol <soloolol@naver.com>
2022-09-02 15:39:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ const app = express()
 const port = 3000
 
 //http 통신프로토콜을 위한 코드
-app.use(    ({
-    Origin : 'https://nunutest.shop/'
-}))
+// app.use(    ({
+//     Origin : 'https://nunutest.shop/'
+// }))
 
-// app.use(cors(
-// ))
+ app.use(cors({
+     Origin : 'https://nunutest.shop/'
+    }
+ ))
 
 
 
