e57cdfaec2b75cfd52610d67858802143bc1b7a8
Hjyuni <hjy072911@gmail.com>
2022-09-15 00:10:26 +0900

    server test

--- index_test.js
+++ index_test.js
@@ -42,7 +42,7 @@ connetion.connect()
 // social Test - kakao
 app.use('/kakao', express.json())
 app.post('/kakao',async (req,res)=>{
-    console.log(req.body)
+    console.log(req.body.properties.thumnail_image)
     let selectUser = await connetion.query(`SELECT id FROM users WHERE id = ?`,[req.body.id])
     if (selectUser[0][0] === undefined){
        let connection = await mysql.createConnection({host: 'localhost',user: 'root',password: '',database: ''})
@@ -57,197 +57,58 @@ app.post('/kakao',async (req,res)=>{
     let data = {
         id: req.body.id,
         nickname: req.body.properties.nickname,
-        thumnail: req.body.properties.thumnail_image
+        thumbnail: req.body.properties.thumbnail_image
     }
     res.send(data)
 });
 app.use('/naver', express.json())
 app.post('/naver',(req,res)=>{
     console.log(req.body)
+    res.send('naver!!')
 });
-
-//-------------------------------------------------
-//테스트용 입력
-
-// app.post('/',function(req, res) {
-//     //res.send('Hello World!')
-//     connetion.query('select * from userinfo', function(error, row){
-//         if(error) throw error
-//         console.log('user info is:', row)
-//         res.send(row)
-//     })
-// })
-
-// app.get('/', function(req, res){
-//     res.send('hello world!')
-// })
-
-// app.post('/user/image', function(req, res){
-//     res.sendFile(__dirname + '/image.jpg', function(error){
-//         if(error) throw error
-//         console.log('send image')
-//     })
-// })
-
-// app.post('/user', function(req, res){
-
-//     //sendFile함수에서 파일경로를 절대경로를 요구함
-//     //__dirname으로 디렉토리까지의 절대경로를 가져옴
-//     res.sendFile(__dirname + '/seoul_kangnam_gu.json', function(error){
-//         if(error) throw error
-//         console.log('send image')
-//     })
-// })
-
 //-----------------------------------------------------
 //프로젝트용 소스코드
-
 
 //express 서버 실행
 app.listen(port, function () {
     console.log(`Example app listen on port ${port}`)
 })
 
-
 //날씨 응답
+app.use('/weather/dailyWeather', express.json())
+app.use('/weather/dailyWeather', express.urlencoded({extended : true}))
 app.post('/weather/dailyWeather', function (req, res) {
-    res.sendFile(__dirname + '/dailyWeather.json')
+
+    let location = req.body.location
+    let data = []
+
+    let jsonFileData = fs.readFileSync('./Project_Crawler/dailyWeather.json')
+    let jsonData = JSON.parse(jsonFileData)
+
+    location = location.replaceAll(' ','')
+
+    for(let i=0;i<jsonData.length;i++)
+    {
+        if(location == jsonData[i].location)
+        {
+            data.push(jsonData[i])
+        }
+    }
+
+    //console.log(data)
+    // data.push({"location":"서해북부앞바다","day":"26일(금)","weather":"비","time":"오후","windDir":"북서-북","windSpeed":"4~9","seaHeight":"0.5~1"})
+    // data.push({"location":"서해북부앞바다","day":"27일(토)","weather":"맑음","time":"오전","windDir":"북서-북","windSpeed":"5~9","seaHeight":"0.5~0.5"})
+    // data.push({"location":"서해북부앞바다","day":"27일(토)","weather":"맑음","time":"오후","windDir":"서-북서","windSpeed":"5~8","seaHeight":"0.5~1"})
+    // data.push({"location":"서해북부앞바다","day":"28일(일)","weather":"구름많음","time":"오전","windDir":"북-북동","windSpeed":"3~6","seaHeight":"0.5~0.5"})
+    // data.push({"location":"서해북부앞바다","day":"28일(일)","weather":"흐림","time":"오후","windDir":"서-북서","windSpeed":"2~4","seaHeight":"0.5~0.5"})
+
+    res.send(data)
+    //res.sendFile(__dirname + '/dailyWeather.json')
     console.log('send daily weather file')
 })
 
 
-
-
-//지도 기반 유저 물고기 사진 띄우기
-app.post('/mapFish/userBase', function (req, res) {
-    //여러개의 이미지 파일 한번에 전송하기
-
-    let zip = new JSZip()
-
-    zip.file("image.jpg", fs.readFileSync(__dirname + '/image.jpg'))
-
-    //zip 파일 생성 in nodejs
-    //참조링크 : https://stuk.github.io/jszip/documentation/howto/write_zip.html
-    zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-        .pipe(fs.createWriteStream('test.zip'))
-        .on('finish', function () {
-            console.log('zip file written')
-        })
-
-    res.sendFile(__dirname + '/test.zip')
-})
-
-
-
-
-
-async function runPythonLength(path)
-{
-    return await new Promise(resolve => {
-        let option = {
-            mode : 'text',
-            pythonPath : '',
-            pythonOptions : ['-u'],
-            scripPath : '',
-            args : [path],
-            encoding : 'utf8'
-        }
-    
-        let returnData = {}
-    
-        pythonShell.PythonShell.run('checkFishLength.py', option, function(err, result){
-            if(err) console.log(err)
-            else {
-                let data = result
-                //let text = data.toString('utf-8')
-                //console.log('run')
-                //console.log(data)
-    
-                console.log('height ' + data[0].split(':')[1].replaceAll(' ', '').replaceAll('cm',''))
-                console.log('width' + data[1].split(':')[1].replaceAll(' ', '').replaceAll('cm',''))
-    
-                returnData.height = data[0].split(':')[1].replaceAll(' ', '').replaceAll('cm','')
-                returnData.width = data[1].split(':')[1].replaceAll(' ', '').replaceAll('cm','')
-            
-                //return resolve(result)
-                //return returnData
-                return resolve(returnData)
-            }
-        })
-    
-        // pythonShell.PythonShell.run('checkFishType.py', option, function(err, result){
-        //     if(err) console.log(err)
-        //     else {
-        //         let data = result
-        //         //let text = data.toString('utf-8')
-        //         //console.log('run')
-        //         //console.log(data)
-    
-        //         console.log(data[2].split(':')[1])
-        //         returnData.type = data[2].split(':')[1]
-        //     }
-        // })
-    
-        console.log("await end")
-        
-    })
-}
-
-async function runPythonType(path) {
-    return await new Promise(resolve => {
-        let option = {
-            mode : 'text',
-            pythonPath : '',
-            pythonOptions : ['-u'],
-            scripPath : '',
-            args : [path],
-            encoding : 'utf8'
-        }
-    
-        let returnData = {}
-    
-        // pythonShell.PythonShell.run('checkFishLength.py', option, function(err, result){
-        //     if(err) console.log(err)
-        //     else {
-        //         let data = result
-        //         //let text = data.toString('utf-8')
-        //         //console.log('run')
-        //         //console.log(data)
-    
-        //         console.log(data[0].split(':')[1].replaceAll(' ', '').replaceAll('cm',''))
-        //         console.log(data[1].split(':')[1].replaceAll(' ', '').replaceAll('cm',''))
-    
-        //         returnData.height = data[0].split(':')[1].replaceAll(' ', '').replaceAll('cm','')
-        //         returnData.width = data[1].split(':')[1].replaceAll(' ', '').replaceAll('cm','')
-            
-        //         //return resolve(result)
-        //         //return returnData
-        //         return resolve(returnData)
-        //     }
-        // })
-    
-        pythonShell.PythonShell.run('checkFishType.py', option, function(err, result){
-            if(err) console.log(err)
-            else {
-                let data = result
-                //let text = data.toString('utf-8')
-                //console.log('run')
-                //console.log(data)
-    
-                console.log(data[2].split(':')[1])
-                returnData.type = data[2].split(':')[1]
-
-                return resolve(returnData)
-            }
-        })
-    
-        console.log("await end")
-        
-    })
-}
-
 //물고기 종류, 길이 판정 수신, 전송
-
 //물고기 정보 수신, echo로 되돌려줌
 
 //파일처리를 위한 multer모듈 설정
@@ -255,7 +116,7 @@ const upload = multer({ dest: 'uploads/' })
 const cpUpload = upload.fields([{ name: 'fish', maxCount: 1 }])
 
 //물고기 정보 수신 and 송신
-app.post('/matchFish/caculateData', cpUpload, async function (req, res) {
+app.post('/matchFish/caculateData', cpUpload, warp(async function (req, res) {
 
     console.log("receive image file data")
 
@@ -277,87 +138,24 @@ app.post('/matchFish/caculateData', cpUpload, async function (req, res) {
         if(error) throw error
     })
 
-    //let data = fs.readFileSync(newPath, "base64")
-
-
     let data = fs.readFileSync(newPath, function (err) {
         if (err) throw err
     })
 
-    // result.stdout.on('data', function(data) {
-    //     console.log(data.toString(''))
-    // })
-
-    // result.stderr.on('data', function(data) {
-    //     console.log(data.toString() + ' err')
-    // })
-
     let pythonDataLength = await runPythonLength(newPath)
     let pythonDataType = await runPythonType(newPath)
 
-    //console.log(pythonData)
-    console.log("test")
-    //console.log(pythonData)
+    console.log("caculate Fish is done")
 
-    // let option = {
-    //     mode : 'text',
-    //     pythonPath : '',
-    //     pythonOptions : ['-u'],
-    //     scripPath : '',
-    //     args : [req.files['fish'][0].path + '.jpg'],
-    //     encoding : 'utf8'
-    // }
-
-    // pythonShell.PythonShell.run('checkFishLength.py', option, function(err, result){
-    //     if(err) console.log(err)
-    //     else {
-    //         let data = result
-    //         //let text = data.toString('utf-8')
-    //         //console.log('run')
-    //         //console.log(data)
-
-    //         console.log(data[0].split(':')[1].replaceAll(' ', '').replaceAll('cm',''))
-    //         console.log(data[1].split(':')[1].replaceAll(' ', '').replaceAll('cm',''))
-    //     }
-    // })
-
-    // pythonShell.PythonShell.run('checkFishType.py', option, function(err, result){
-    //     if(err) console.log(err)
-    //     else {
-    //         let data = result
-    //         //let text = data.toString('utf-8')
-    //         //console.log('run')
-    //         //console.log(data)
-
-    //         console.log(data[2].split(':')[1])
-    //     }
-    // })
-
-    // let length = Math.random() * 100
-    //let fishType = '열대어'
     let imageName = req.files['fish'][0].path + '.jpg'
-
     let length = parseFloat(pythonDataLength.height) > parseFloat(pythonDataLength.width) ? pythonDataLength.height : pythonDataLength.width
     let fishType = pythonDataType.type
 
-    //length = length.toFixed(2).toString() + 'cm'
     imageName = imageName.split('\\')[1]
-    //console.log(imageName)
-
 
     // connetion.query('insert into catchFishData (user, fishType, fishLength, latitude, longitude, imagePath) values (?,?,?,?,?,?)', ['test', fishType, length, location.latitude, location.longitude, imageName], function(err, row, filed) {
     //     if(err) console.log(err)
     // })
-
-    //res.send(data)
-
-    //send base64
-    //let sendData = new Buffer.from(data).toString("base64")
-    //console.log(sendData)
-    //res.send(sendData)
-
-   
-    
 
     let sendData = {}
 
@@ -366,40 +164,8 @@ app.post('/matchFish/caculateData', cpUpload, async function (req, res) {
     sendData.imageData = new Buffer.from(data).toString("base64")
 
     res.send(sendData)
-    //send pack blob
-    // console.log(data)
-    // let blob = new Blob([data], {type:'image/jpeg'})
-    // console.log(blob)
-
-    // //물고기사진 전송
-
-    // res.type(blob.type)
-    // blob.arrayBuffer().then((buf) =>{
-    //     res.send(Buffer.from(buf))
-    // })
-
-    //send raw binaly
-    //res.sendFile(newPath)
-})
-
-app.get('/matchFish/caculateData', function (req, res) {
-    let image = __dirname + '/image.jpg'
-    res.sendFile(image)
-
-    console.log('send image')
-})
-
-//text filed 테스트, x-www-form-urlencoded형태의 데이터를 인식
-app.use('/matchFish.receiveData', express.json())
-app.use('/matchFish/receiveData', express.urlencoded({ extended: true }))
-app.post('/matchFish/receiveData', function (req, res) {
-    console.log('for body test')
-
-    console.log(req.body)
-
-    res.send(req.body)
-})
-
+    console.log("send data")
+}))
 
 // rank전송용
 app.use('/rank/fish', express.json())
@@ -413,17 +179,45 @@ app.post('/rank/fish', function (req, res) {
 
     if(req.body.fishType == '참돔')
     {
-        data.push({ 'rank': 1, 'length': 15 })
-        data.push({ 'rank': 2, 'length': 14 })
-        data.push({ 'rank': 3, 'length': 13 })
-        data.push({ 'rank': 4, 'length': 12 })
+        data.push({ 'rank': 1, 'id': 'test1', 'length': 15, 'grade' : 'A' })
+        data.push({ 'rank': 2, 'id': 'test2', 'length': 14, 'grade' : 'B' })
+        data.push({ 'rank': 3, 'id': 'test3', 'length': 12, 'grade' : 'D' })
+        data.push({ 'rank': 4, 'id': 'test4', 'length': 10, 'grade' : 'E' })
+    }
+    else if(req.body.fishType == '돌돔')
+    {
+        data.push({ 'rank': 21, 'id': 'test1', 'length': 51, 'grade' : 'A' })
+        data.push({ 'rank': 31, 'id': 'test2', 'length': 41, 'grade' : 'B' })
+        data.push({ 'rank': 41, 'id': 'test3', 'length': 21, 'grade' : 'C' })
+        data.push({ 'rank': 51, 'id': 'test4', 'length': 15, 'grade' : 'F' })
+    }
+    else if(req.body.fishType == '감성돔')
+    {
+        data.push({ 'rank': 15, 'id': 'test1', 'length': 33, 'grade' : 'A' })
+        data.push({ 'rank': 17, 'id': 'test2', 'length': 31, 'grade' : 'B' })
+        data.push({ 'rank': 19, 'id': 'test3', 'length': 29, 'grade' : 'C' })
+        data.push({ 'rank': 21, 'id': 'test4', 'length': 27, 'grade' : 'D' })
+    }
+    else if(req.body.fishType == '조피볼락')
+    {
+        data.push({ 'rank': 10, 'id': 'test1', 'length': 22, 'grade' : 'A' })
+        data.push({ 'rank': 11, 'id': 'test2', 'length': 21, 'grade' : 'A' })
+        data.push({ 'rank': 12, 'id': 'test3', 'length': 20, 'grade' : 'A' })
+        data.push({ 'rank': 13, 'id': 'test4', 'length': 18, 'grade' : 'B' })
+    }
+    else if(req.body.fishType == '넙치')
+    {
+        data.push({ 'rank': 31, 'id': 'test1', 'length': 29, 'grade' : 'A' })
+        data.push({ 'rank': 33, 'id': 'test2', 'length': 26, 'grade' : 'C' })
+        data.push({ 'rank': 36, 'id': 'test3', 'length': 22, 'grade' : 'E' })
+        data.push({ 'rank': 38, 'id': 'test4', 'length': 15, 'grade' : 'F' })
     }
     else
     {
-        data.push({ 'rank': 10, 'length': 51 })
-        data.push({ 'rank': 11, 'length': 41 })
-        data.push({ 'rank': 12, 'length': 31 })
-        data.push({ 'rank': 13, 'length': 21 })
+        data.push({ 'rank': 1, 'id': 'err1', 'length': 15, 'grade' : 'A' })
+        data.push({ 'rank': 2, 'id': 'err2', 'length': 14, 'grade' : 'B' })
+        data.push({ 'rank': 3, 'id': 'err3', 'length': 12, 'grade' : 'D' })
+        data.push({ 'rank': 4, 'id': 'err4', 'length': 10, 'grade' : 'E' })
     }
 
 
@@ -442,18 +236,17 @@ app.get('/map/fish', function (req, res) {
     })
 
     let sendData = new Buffer.from(data).toString("base64")
-    //console.log(sendData)
+
     res.send(sendData)
 })
 
 app.use('/map/center', express.json())
 app.use('/map/center', express.urlencoded({ extended: true }))
 app.post('/map/center', async function (req, res) {
-    console.log('mapCenter')
-
+    
     let data = req.body
 
-    console.log(data.Ma + ' ' + data.La) 
+    console.log('map Center : ' + data.Ma + ' ' + data.La) 
 
     //get position data for database with data, get image Path list
 
@@ -465,31 +258,26 @@ app.post('/map/center', async function (req, res) {
     for(let i=0;i<selectData.length;i++){
         let packData = {}
 
-        //let filePath = __dirname + '/image' + i + '.jpg'
-        let filePath = __dirname + '/uploads/' + selectData[i].imagePath
-        let imageData = fs.readFileSync(filePath, function(err) {
-            if(err) throw err
-        })
+        //if(parseFloat(selectData[i].latitude) )
+        if(caculateLocation(data.Ma, data.La, selectData[i].latitude, selectData[i].longitude)) {
 
-        //packData.latitude = data.La
-        //packData.longitude = data.Ma
-        //packData.fishType = '열대어'
-        //packData.fishLength = (Math.random() * 100).toFixed(1).toString() + 'cm'
-        
-        packData.latitude = selectData[i].latitude
-        packData.longitude = selectData[i].longitude
-        packData.fishType = selectData[i].fishType
-        packData.fishLength = selectData[i].fishLength
-        packData.image = new Buffer.from(imageData).toString("base64")
+            //let filePath = __dirname + '/image' + i + '.jpg'
+            let filePath = __dirname + '/uploads/' + selectData[i].imagePath
+            let imageData = fs.readFileSync(filePath, function(err) {
+                if(err) throw err
+            })
+            
+            packData.latitude = selectData[i].latitude
+            packData.longitude = selectData[i].longitude
+            packData.fishType = selectData[i].fishType
+            packData.fishLength = selectData[i].fishLength
+            packData.image = new Buffer.from(imageData).toString("base64")
 
-        console.log(i)
-
-        sendData.push(packData)
+            sendData.push(packData)
+        }
     }
 
-    console.log('end')
-
-    //sendData.push(data)
-
     res.send(sendData)
+    console.log('send MapFishData')
 })
+
