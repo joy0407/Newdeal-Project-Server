0dee6aace03789868efa078af414f35f5b7c438d
Hjyuni <hjy072911@gmail.com>
2022-09-15 10:54:57 +0900

    modify

--- index_test.js
+++ index_test.js
@@ -3,9 +3,9 @@ import mysql from "mysql2/promise"
 import path from "path"
 import multer from "multer"
 import fs from "fs"
-import JSZip from "jszip"
 import cors from  "cors"
-import pythonShell from "python-shell"
+import {runPythonLength, runPythonType} from "./pythonJS.js"
+import {caculateLocation} from "./functionJS.js"
 
 
 //module타입 코딩에서는 __dirname이 정의되어있지않음, 수동으로 직접 정의
@@ -14,7 +14,6 @@ const __dirname = path.resolve()
 //express 초기화
 const app = express()
 const port = 3000
-
 
 // accesss allow url list (CORS : 통신 프로토콜이 서로 다를때 헤더에 담아 허가해줌) 
 // 다음 함수 실행으로 header 에 Access-Control-Allow-Origin:'https://nunutest.shop' 데이터를 서브해 줄 클라이언트
@@ -29,7 +28,7 @@ app.use(cors({
 
 
 
-// Mysql 연결설정
+//Mysql 연결설정
 const connetion = await mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -80,6 +79,82 @@ app.post('/naver',async (req,res)=>{
     }
     res.send(data)
 });
+
+//await/async error핸들링용 warp함수
+const warp = function(fn) {
+    return async function(res, req, next) {
+        try{
+            await fn(res, req, next)
+        }
+        catch(err){
+            next(err)
+        }
+    }
+}
+
+//-------------------------------------------------
+//테스트용 입력
+
+// app.post('/',function(req, res) {
+//     //res.send('Hello World!')
+//     connetion.query('select * from userinfo', function(error, row){
+//         if(error) throw error
+//         console.log('user info is:', row)
+//         res.send(row)
+//     })
+// })
+
+// app.get('/', function(req, res){
+//     res.send('hello world!')
+// })
+
+// app.post('/user/image', function(req, res){
+//     res.sendFile(__dirname + '/image.jpg', function(error){
+//         if(error) throw error
+//         console.log('send image')
+//     })
+// })
+
+// app.post('/user', function(req, res){
+
+//     //sendFile함수에서 파일경로를 절대경로를 요구함
+//     //__dirname으로 디렉토리까지의 절대경로를 가져옴
+//     res.sendFile(__dirname + '/seoul_kangnam_gu.json', function(error){
+//         if(error) throw error
+//         console.log('send image')
+//     })
+// })
+
+// //text filed 테스트, x-www-form-urlencoded형태의 데이터를 인식
+// app.use('/matchFish.receiveData', express.json())
+// app.use('/matchFish/receiveData', express.urlencoded({ extended: true }))
+// app.post('/matchFish/receiveData', function (req, res) {
+//     console.log('for body test')
+
+//     console.log(req.body)
+
+//     res.send(req.body)
+// })
+
+// //지도 기반 유저 물고기 사진 띄우기
+// app.post('/mapFish/userBase', function (req, res) {
+//     //여러개의 이미지 파일 한번에 전송하기
+
+//     let zip = new JSZip()
+
+//     zip.file("image.jpg", fs.readFileSync(__dirname + '/image.jpg'))
+
+//     //zip 파일 생성 in nodejs
+//     //참조링크 : https://stuk.github.io/jszip/documentation/howto/write_zip.html
+//     zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+//         .pipe(fs.createWriteStream('test.zip'))
+//         .on('finish', function () {
+//             console.log('zip file written')
+//         })
+
+//     res.sendFile(__dirname + '/test.zip')
+// })
+
 //-----------------------------------------------------
 //프로젝트용 소스코드
 
@@ -294,4 +369,3 @@ app.post('/map/center', async function (req, res) {
     res.send(sendData)
     console.log('send MapFishData')
 })
-
