aac8508312d60f5aab34de00a4bbfc34f3079f7b
joy0407 <joy040712@naver.com>
2022-09-16 14:48:56 +0900

    file

--- functionJS.js
+++ functionJS.js
@@ -1,10 +1,10 @@
 
 //전달받은 두 값이 중심값(center)를 기준으로 일정거리내에 있으면 true를 반환하는 함수
 //판별은 현재 사각형을 기준으로 판별
-//현재 기준은 0.5
+//현재 기준은 1.0
 function caculateLocation(centerLatitude, centerLongitude, currentLatitude, currentLongitude) {
-    if(Math.abs(parseFloat(centerLatitude) - parseFloat(currentLatitude)) < parseFloat(0.5) &&
-        Math.abs(parseFloat(centerLongitude) - parseFloat(currentLongitude)) < parseFloat(0.5))
+    if(Math.abs(parseFloat(centerLatitude) - parseFloat(currentLatitude)) < parseFloat(1.0) &&
+        Math.abs(parseFloat(centerLongitude) - parseFloat(currentLongitude)) < parseFloat(1.0))
         return true
     else
         return false
--- print.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-
-if __name__ == '__main__':
-    utf8stdout = open(1, 'w', encoding='utf-8', closefd=False)
-    print("1 + 1 = 2 입니다.", file=utf8stdout)
-    sys.stdout.flush()
